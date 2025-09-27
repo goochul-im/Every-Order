0b351a461835671fce30c9b27abd8fa84f795144
goochul-im <goochul175465@gmail.com>
2025-09-27 21:40:28 +0900

    :tada: feat(project): start project

---
+++ src/test/kotlin/com/everyorder/EveryOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.everyorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EveryOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
